ed095f265055fc0183d1be2e5e9db5a4e648626a
MekAniml <m3kaniml@gmail.com>
2022-04-19 00:39:56 +0200

    include json-c for parsing

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <string.h>
+#include <json-c/json.h>
 
+
+/*void parsingData(char *weather, char *iconId, int *temp, char *file_name)
+{
+    //parse the file file_name and extract the data for "description", "icon" and "temp
+    //and store them in weather, iconId and temp
+    FILE *fp = fopen(file_name, "r");
+
+
+}*/
 
 void get_page(char *url, char *file_name){
     CURL* easyhandle = curl_easy_init();
@@ -20,10 +30,36 @@ void get_page(char *url, char *file_name){
 
 }
 
+void parseJson(char *file_name) {
+    //parse the file file_name and extract the data for "description", "icon" and "temp
+    //and store them in weather, iconId and temp
+    FILE *fp = fopen(file_name, "r");
+    char buffer[1024];
+    struct json_object *parsed_json;
+    struct json_object *description;
+    struct json_object *icon;
+    struct json_object *temp_obj;
+
+    fread(buffer, 1, sizeof(buffer), fp);
+    fclose(fp);
+
+    //parse the json
+    parsed_json = json_tokener_parse(buffer);
+    json_object_object_get_ex(parsed_json, "description", &description);
+    json_object_object_get_ex(parsed_json, "icon", &icon);
+    json_object_object_get_ex(parsed_json, "temp", &temp_obj);
+    //extract the data
+    printf("%s\n", json_object_to_json_string(description));
+    printf("%s\n", json_object_to_json_string(icon));
+    printf("%s\n", json_object_to_json_string(temp_obj));
+
+
+}
+
 int main() {
     char *filename = "../http.txt"; //path from current dir
 
-    get_page( "https://api.openweathermap.org/data/2.5/weather?q=Lyon&appid=a74be1f8fd103c83ce4e4c545a33c915&units=metric&lang=fr&mode=xml", filename ) ;
-
+    get_page( "https://api.openweathermap.org/data/2.5/weather?q=Lyon&appid=a74be1f8fd103c83ce4e4c545a33c915&units=metric&lang=fr&mode=json", filename ) ;
+    parseJson(filename);
     return 0;
 }
